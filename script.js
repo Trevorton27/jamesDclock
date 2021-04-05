95672e296c21e58bd316f9e6a7142c4396ffe8e1
Trevor Mearns <spiral272@gmail.com>
2021-04-05 13:54:51 +0900

    ready for code review approval

--- script.js
+++ script.js
@@ -1,53 +1,83 @@
-function clock (){
-  let date = new Date();
-  let hrs = date.getHours();
-  let mins = date.getMinutes();
-  let secs = date.getSeconds();
-  let period = "AM";
-  
- 
-  if(hrs == 0){
-    hrs = 12;
-  }if(hrs > 12){
-    hrs = hrs -12;
-    period = "PM";
-  }
- if (hrs <10){
-   hrs = "0" + hrs;
- }
- if (mins < 10){
-   mins = "0" + mins;
- }
- if (secs < 10){
-   secs = "0" + secs;
- }
+function clock() {
+  const time = new Date();
+  let hours = time.getHours();
+  const minutes = addLeadingZero(time.getMinutes());
+  const seconds = addLeadingZero(time.getSeconds());
+  const isAm = hours < 12 || hours === 0;
+  let amPm = isAm ? 'AM' : 'PM';
 
-  let time = `${hrs}:${mins}:${secs}:${period}`;
-  document.getElementById("clock").innerText = time;
-
-
-  let months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let wd = days[date.getDay()];
-  let dd = date.getDate();
-  let mm = months[date.getMonth()];
-  let yyyy = date.getFullYear();
- 
-  
-   if (dd === 01 || dd === 21 || dd === 31) {
-      dd = dd + "st";
-  } else if (dd === 02 || dd === 22) {
-      dd = dd + "nd";
-  } else if (dd === 03 || dd === 23) {
-      dd = dd + "rd";
-  } else {
-      dd = dd + "th";
-  }
-  
-  let calendar = `${wd}, ${mm} ${dd} ${yyyy}`; 
-
-  document.getElementById("date").innerText = calendar;
+  let timeDisplay = `${renderTwelveHourClock(
+    hours
+  )}:${minutes}:${seconds}:${amPm}`;
+  document.getElementById('clock').innerText = timeDisplay;
 }
-  setInterval(clock, 100);
+
+function addDateSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
+  }
+  return `${date}th`;
+}
+
+function addLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+function renderTwelveHourClock(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
+
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
+}
+
+function date() {
+  const date = new Date();
+
+  const day = days[date.getDay()];
+  const dateToday = addDateSuffix(date.getDate());
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  const calendar = `${day}, ${month} ${dateToday} ${year}`;
+
+  document.getElementById('date').innerText = calendar;
+}
+
+let months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+let days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+setInterval(() => {
   clock();
+  date();
+}, 1000);
+
+clock();
+date();
